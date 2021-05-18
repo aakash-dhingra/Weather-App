c0f7dd2c8fd97ed44f7b0879dc7c33f8b5ac95bc
aakash-dhingra <54613654+aakash-dhingra@users.noreply.github.com>
2021-05-19 00:26:36 +0530

    First

---
+++ app.js
@@ -0,0 +1,78 @@
+const input = document.querySelector('input');
+const button = document.getElementById('btn');
+var x = document.querySelector('.first');
+const h1 = document.querySelectorAll('h1');
+
+const api = {
+    apiKey: '3090905c3d1ddc992f0412f2672094ea',
+    url: 'api.openweathermap.org/data/2.5/'
+}
+
+
+
+btn.addEventListener('click', (e) => {
+    console.log(e);
+    e.preventDefault();
+    console.dir(input);
+    let cityName = input.value;
+    const data = findWeather(cityName);
+    x.classList.add("second");
+    input.value = "";
+})
+
+input.addEventListener('keypress',function (e) {
+    let cityName = input.value;
+    if (e.keyCode === 13) {
+        findWeather(cityName);
+        x.classList.add("second");
+        input.value = "";
+    }
+})
+
+
+
+
+function findWeather(city) {
+    let cityName = input.value;
+    fetch(`https://${api.url}weather?q=${city}&units=metric&APPID=${api.apiKey}`)
+        .then((apiData) => {
+            console.log(apiData);
+            return apiData.json();
+        }).then((data) => {
+            // do stuff with the data
+            console.dir(data);
+            let name=data.name;
+            let country = data.sys.country;
+            let temp = data.main.temp;
+            let max_temp= data.main.temp_max;
+            let min_temp= data.main.temp_min;
+            let type = data.weather[0].main;
+            let iconcode = data.weather[0].icon;
+            let iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+            // console.log(name);
+            // console.log(country);
+            // console.log(temp);
+            // console.log(max_temp);
+            // console.log(min_temp);
+            // console.log(type);
+            h1[1].innerHTML=`${name}, <span id="cnt">${country}<span>`;
+            h1[2].innerText=currentDate();
+            h1[3].innerHTML=`${Math.round(temp)}<span>&#176;C </span>`
+            h1[4].innerText=`${type}`;
+            h1[5].innerHTML=`<img src=${iconurl} alt="Image"/>`;
+            h1[6].innerHTML=`<span id="min">${Math.round(min_temp)}</span><span>&#176;C </span> /  <span id="max">${Math.round(max_temp)}</span><span>&#176;C </span>`
+            // console.log(country);
+            div.style.display="visible";
+        })
+}
+
+
+function currentDate() {
+    var WeekDay = new Date;
+    let week = ["Sunday", "Monday", "Tuesday", "Wednday", "Thrusday", "Friday", "Saturday"];
+    let month = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let day = WeekDay.getDate();
+    return `${week[WeekDay.getDay()]}- ${day}  ${month[WeekDay.getMonth()]},${WeekDay.getFullYear()}`;
+}
+// ---------------------------------using axios-----------------------------------------------
+
